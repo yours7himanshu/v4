b165a9d092a55563eaca40a5f80eb23a811acbe4
Aleksey Razbakov <aleksey@razbakov.com>
2025-01-04 02:00:49 +0100

    fix youtube preview

--- components/StyleCard.vue
+++ components/StyleCard.vue
@@ -1,35 +1,35 @@
 <script setup lang="ts">
 interface Style {
-  name: string
-  image: string
-  to: string
-  members: number
+  name: string;
+  image: string;
+  to: string;
+  members: number;
 }
 
 defineProps<{
-  style: Style
-}>()
+  style: Style;
+}>();
 </script>
 
 <template>
-  <NuxtLink 
+  <NuxtLink
     :to="style.to"
     class="group relative aspect-[4/3] overflow-hidden rounded-xl bg-white/10 hover:bg-white/20 backdrop-blur-sm transition"
   >
-    <NuxtImg 
-      :src="style.image" 
+    <NuxtImg
+      :src="style.image"
       :alt="style.name"
       fit="cover"
-      class="absolute inset-0 w-full h-full object-fill opacity-50 group-hover:opacity-70 transition"
+      class="absolute inset-0 w-full h-full object-cover scale-[1.5] opacity-50 group-hover:opacity-70 transition"
     />
-    <div class="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent"></div>
+    <div
+      class="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent"
+    ></div>
     <div class="relative h-full p-4 flex flex-col justify-end text-left">
       <h3 class="font-bold text-xl text-white">{{ style.name }}</h3>
       <div class="text-sm text-white/80">
-        <p class="flex items-center gap-1">
-          {{ style.members }} members
-        </p>
+        <p class="flex items-center gap-1">{{ style.members }} members</p>
       </div>
     </div>
   </NuxtLink>
-</template> 
+</template>
