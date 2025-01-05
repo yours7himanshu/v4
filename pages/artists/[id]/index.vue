ac973d9f69005d1252da82a330afe260b8b1023b
Aleksey Razbakov <aleksey@razbakov.com>
2025-01-05 05:32:50 +0100

    fix artists

--- pages/artists/[id]/index.vue
+++ pages/artists/[id]/index.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import type { ArtistProfile } from "~/schemas/profile";
-import Feed from "~/components/Feed.vue";
 import EmptyState from "~/components/common/EmptyState.vue";
 
 const { artist, artistId } = useArtist();
@@ -9,7 +7,7 @@ const { artist, artistId } = useArtist();
 <template>
   <ArtistLayout v-if="artist" :artist="artist">
     <div class="max-w-xl mx-auto">
-      <Feed :type="'all'" :limit="3" :author-id="artistId" />
+      <PostList :type="'all'" :limit="3" :author-id="artistId" />
     </div>
   </ArtistLayout>
 
