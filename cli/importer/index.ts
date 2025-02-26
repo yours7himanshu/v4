9ecc6a7bfb33e6d4b6b74190f1ad0d0de35c6333
Aleksey Razbakov <aleksey@razbakov.com>
2025-02-26 11:28:10 +0100

    add database (#83)
    * add database
    
    * update Makefile
    
    * update cli
    
    * env examples
    
    * update readme #62

---
+++ cli/importer/index.ts
@@ -0,0 +1,334 @@
+import { sortBy } from "lodash";
+import { getProfiles } from "../provider/profiles";
+import { getAccounts, getUsers, getSuspended } from "../provider/accounts";
+import { getDanceStyles } from "../provider/styles";
+import { getEvents } from "../provider/events";
+import { getPosts } from "../provider/posts";
+import { addEvent } from "./event";
+import { addPost } from "./post";
+import {
+  addProfile,
+  addCity,
+  addFollowers,
+  addCitySubscribers,
+} from "./profile";
+import { addAccount } from "./account";
+import { addDanceStyle, clearVotes } from "./style";
+import * as cliProgress from "cli-progress";
+import { getLogger } from "../utils/logger";
+
+export async function importDanceStyles(multibar: cliProgress.MultiBar) {
+  let collection = "styles";
+  const logger = getLogger(collection);
+  logger.info("Importing dance styles");
+
+  const danceStyles = await getDanceStyles();
+
+  let created = 0;
+  let ignored = 0;
+  let failed = 0;
+  let updated = 0;
+
+  const bar = multibar.create(danceStyles.length, 0, {
+    collection,
+    created,
+    ignored,
+    failed,
+    updated,
+  });
+
+  await clearVotes();
+
+  for (const danceStyle of danceStyles) {
+    logger.debug("importing", danceStyle.id);
+
+    const result = await addDanceStyle(danceStyle);
+
+    if (result.state === "created") {
+      created++;
+    } else if (result.state === "ignored") {
+      ignored++;
+    } else if (result.state === "failed") {
+      failed++;
+    } else if (result.state === "updated") {
+      updated++;
+    }
+
+    bar.increment({ created, ignored, failed, updated });
+  }
+
+  bar.stop();
+
+  logger.info("Finished importing dance styles", {
+    total: danceStyles.length,
+    created,
+    ignored,
+    failed,
+    updated,
+  });
+}
+
+export async function importAccounts(multibar: cliProgress.MultiBar) {
+  let collection = "accounts";
+  const logger = getLogger(collection);
+  logger.info("Importing accounts");
+
+  const accounts = await getAccounts();
+  const users = getUsers();
+  const suspended = getSuspended();
+
+  let created = 0;
+  let ignored = 0;
+  let failed = 0;
+
+  const bar = multibar.create(accounts.length, 0, {
+    collection,
+    updated: 0,
+  });
+
+  for (const account of accounts) {
+    logger.debug({
+      state: "importing",
+      id: account.id,
+      email: account.email,
+    });
+
+    const user = users.find((u: any) => u.localId === account.id);
+    const isSuspended = suspended.find((s: any) => s.uid === account.id);
+    bar.increment({ failed, created, ignored });
+
+    const result = await addAccount(account, user, isSuspended);
+
+    if (result.state === "created") {
+      created++;
+    }
+
+    if (result.state === "ignored") {
+      ignored++;
+    }
+
+    if (result.state === "failed") {
+      failed++;
+    }
+
+    logger.log(result.state === "failed" ? "error" : "debug", result);
+  }
+
+  bar.stop();
+}
+
+export async function importCities(multibar: cliProgress.MultiBar) {
+  let collection = "cities";
+  const logger = getLogger(collection);
+  logger.info("Importing cities");
+
+  const allProfiles = sortBy(await getProfiles(), "createdAt");
+  const cities = allProfiles.filter((p: any) => p.type === "City");
+
+  let created = 0;
+  let updated = 0;
+  let failed = 0;
+  let ignored = 0;
+
+  const bar = multibar.create(cities.length, 0, {
+    collection,
+  });
+
+  for (const city of cities) {
+    logger.debug({
+      state: "importing",
+      id: city.id,
+      username: city.username,
+    });
+
+    bar.increment({ failed, created, updated, ignored });
+
+    const result = await addCity(city);
+    if (result.state === "updated") {
+      updated++;
+    }
+
+    if (result.state === "ignored") {
+      ignored++;
+    }
+
+    if (result.state === "created") {
+      created++;
+    }
+
+    if (result.state === "failed") {
+      failed++;
+    }
+
+    logger.log(result.state === "failed" ? "error" : "debug", result);
+  }
+
+  bar.stop();
+}
+
+export async function importSubscribers(multibar: cliProgress.MultiBar) {
+  const profiles = sortBy(await getProfiles(), "createdAt");
+  let created = 0;
+  let failed = 0;
+
+  let collection = "subscribers";
+  const logger = getLogger(collection);
+
+  const bar = multibar.create(profiles.length, 0, {
+    collection,
+    ignored: 0,
+    updated: 0,
+  });
+
+  for (const profile of profiles) {
+    let result;
+    if (profile.type === "City") {
+      result = await addCitySubscribers(profile);
+      created += result.created;
+      failed += result.failed;
+    } else {
+      result = await addFollowers(profile);
+      created += result.created;
+      failed += result.failed;
+    }
+
+    for (const error of result.errors) {
+      logger.error(error);
+    }
+
+    bar.increment({ failed, created });
+  }
+  bar.stop();
+}
+
+export async function importProfiles(multibar: cliProgress.MultiBar) {
+  let collection = "profiles";
+  const logger = getLogger(collection);
+  logger.info("Importing profiles");
+
+  let profiles = sortBy(await getProfiles(), ["lastLoginAt", "viewsCount"]);
+  profiles = profiles.filter((p: any) => p.type !== "City");
+
+  let created = 0;
+  let failed = 0;
+  let updated = 0;
+
+  const bar = multibar.create(profiles.length, 0, {
+    collection,
+    ignored: 0,
+  });
+
+  for (const profile of profiles) {
+    logger.debug({
+      state: "importing",
+      id: profile.id,
+      username: profile.username,
+    });
+
+    const result = await addProfile(profile);
+
+    if (result.state === "created") {
+      created++;
+    }
+
+    if (result.state === "updated") {
+      updated++;
+    }
+
+    if (result.state === "failed") {
+      failed++;
+    }
+
+    if (result.state === "ignored") {
+      failed++;
+    }
+
+    logger.log(result.state === "failed" ? "error" : "debug", result);
+
+    bar.increment({ failed, created, updated });
+  }
+  bar.stop();
+}
+
+export async function importPosts(multibar: cliProgress.MultiBar) {
+  let collection = "posts";
+  const logger = getLogger(collection);
+  logger.info("Importing posts");
+
+  let created = 0;
+  let updated = 0;
+  let ignored = 0;
+  let failed = 0;
+
+  const posts = await getPosts();
+
+  const bar = multibar.create(posts.length, 0, {
+    collection,
+  });
+
+  for (const post of posts) {
+    logger.debug("importing", post.id);
+
+    const result = await addPost(post);
+    bar.increment({ created, updated, ignored, failed });
+
+    if (result.state === "created") {
+      created++;
+    }
+    if (result.state === "updated") {
+      updated++;
+    }
+    if (result.state === "ignored") {
+      ignored++;
+    }
+    if (result.state === "failed") {
+      failed++;
+    }
+
+    logger.log(result.state === "failed" ? "error" : "debug", result);
+  }
+
+  bar.stop();
+}
+
+export async function importEvents(multibar: cliProgress.MultiBar) {
+  let collection = "events";
+  const logger = getLogger(collection);
+
+  logger.info("Importing events");
+
+  let created = 0;
+  let updated = 0;
+  let ignored = 0;
+  let failed = 0;
+
+  const events = await getEvents();
+
+  const bar = multibar.create(events.length, 0, {
+    collection,
+  });
+
+  for (const event of events) {
+    logger.debug("importing", event.id);
+
+    const result = await addEvent(event);
+    bar.increment({ created, updated, ignored, failed });
+
+    if (result.state === "created") {
+      created++;
+    }
+    if (result.state === "updated") {
+      updated++;
+    }
+    if (result.state === "ignored") {
+      ignored++;
+    }
+    if (result.state === "failed") {
+      failed++;
+    }
+
+    logger.log(result.state === "failed" ? "error" : "debug", result);
+  }
+
+  bar.stop();
+}
---
+++ cli/importer/profile.ts
@@ -0,0 +1,445 @@
+import { PrismaClient } from "@prisma/client";
+import { getDateOrNow, getDateOrNull } from "../utils/date";
+import * as lookup from "country-code-lookup";
+import { getLogger } from "../utils/logger";
+import { getNormalizedString, getSlug } from "../utils/slug";
+
+const prisma = new PrismaClient();
+
+export async function addCitySubscribers(profile: any) {
+  let created = 0;
+  let failed = 0;
+  let ignored = 0;
+  const errors: any[] = [];
+
+  const usernames = profile.watch?.usernames || [];
+  for (const username of usernames) {
+    const subscriberProfile = await prisma.profile.findFirst({
+      where: { firebaseUsername: username, claimed: true },
+    });
+    if (!profile.cityPlaceId) {
+      failed++;
+
+      errors.push({
+        error: "city not found",
+        state: "failed",
+        profileId: profile.id,
+        profileUsername: profile.username,
+        subscriberUsername: username,
+      });
+
+      continue;
+    }
+    if (!subscriberProfile?.id) {
+      ignored++;
+
+      errors.push({
+        error: "subscriber not found",
+        state: "ignored",
+        profileId: profile.id,
+        profileUsername: profile.username,
+        subscriberUsername: username,
+      });
+
+      continue;
+    }
+
+    try {
+      await prisma.citySubscriber.create({
+        data: {
+          cityId: profile.cityPlaceId,
+          profileId: subscriberProfile.id,
+        },
+      });
+    } catch (error: any) {
+      if (error.code === "P2003") {
+        ignored++;
+
+        errors.push({
+          state: "ignored",
+          error: "subscriber duplicate",
+          profileId: profile.id,
+          profileUsername: profile.username,
+          subscriberUsername: username,
+        });
+        continue;
+      }
+    }
+
+    created++;
+  }
+
+  return {
+    created,
+    failed,
+    errors,
+    ignored,
+  };
+}
+
+export async function addFollowers(profile: any) {
+  let created = 0;
+  let failed = 0;
+  let ignored = 0;
+  const errors: any[] = [];
+
+  const usernames = profile.watch?.usernames || [];
+  for (const username of usernames) {
+    const subscriberProfile = await prisma.profile.findFirst({
+      where: { firebaseUsername: username, claimed: true },
+    });
+    if (!subscriberProfile?.id) {
+      failed++;
+
+      errors.push({
+        error: "follower not found",
+        state: "failed",
+        profileId: profile.id,
+        profileUsername: profile.username,
+        subscriberUsername: username,
+      });
+
+      continue;
+    }
+
+    if (profile.id === subscriberProfile.id) {
+      ignored++;
+
+      errors.push({
+        state: "ignored",
+        error: "self follower",
+        profileId: profile.id,
+        profileUsername: profile.username,
+        subscriberUsername: username,
+      });
+
+      continue;
+    }
+
+    try {
+      await prisma.profileFollower.create({
+        data: {
+          profileId: profile.id,
+          followerId: subscriberProfile.id,
+        },
+      });
+    } catch (error: any) {
+      if (error.code === "P2003") {
+        ignored++;
+
+        errors.push({
+          state: "ignored",
+          error: "follower duplicate",
+          profileId: profile.id,
+          profileUsername: profile.username,
+          subscriberUsername: username,
+        });
+
+        continue;
+      }
+    }
+
+    created++;
+  }
+
+  return {
+    created,
+    failed,
+    errors,
+    ignored,
+  };
+}
+
+function getCountryInternetCode(name: string) {
+  const countryMap: any = {
+    Türkiye: "tr",
+    "Cabo Verde": "cv",
+  };
+
+  if (countryMap[name]) {
+    return countryMap[name];
+  }
+
+  const countryLookup = lookup.byCountry(name);
+  if (!countryLookup) {
+    throw new Error(`Country not found: ${name}`);
+  }
+
+  return countryLookup.internet.toLowerCase();
+}
+
+export async function getCountryCode(name: string) {
+  let country = await prisma.country.findFirst({
+    where: { name },
+  });
+
+  if (country) {
+    return country.code;
+  }
+
+  const code = getCountryInternetCode(name);
+
+  country = await prisma.country.findFirst({
+    where: { code },
+  });
+
+  if (country) {
+    return country.code;
+  }
+
+  const result = await prisma.country.create({
+    data: {
+      code,
+      name,
+    },
+  });
+
+  return result.code;
+}
+
+export async function addCity(profile: any) {
+  if (profile.username === "Travel") {
+    return {
+      state: "ignored",
+      id: profile.id,
+    };
+  }
+
+  if (!profile.location) {
+    return {
+      state: "failed",
+      error: "city has no location",
+      username: profile.username,
+      cityPlaceId: profile.cityPlaceId,
+      id: profile.id,
+    };
+  }
+
+  if (!profile.location.country) {
+    return {
+      state: "failed",
+      error: "city has no country",
+      username: profile.username,
+      cityPlaceId: profile.cityPlaceId,
+      id: profile.id,
+    };
+  }
+
+  if (!profile.location.locality) {
+    return {
+      state: "failed",
+      error: "city has no locality",
+      username: profile.username,
+      cityPlaceId: profile.cityPlaceId,
+      id: profile.id,
+    };
+  }
+
+  const existingPlaceId = await prisma.city.findFirst({
+    where: { id: profile.location.place_id },
+  });
+
+  if (!!existingPlaceId) {
+    return {
+      state: "ignored",
+      id: existingPlaceId.id,
+    };
+  }
+
+  const region = profile.location.region;
+  const country = profile.location.country;
+  const locality = profile.location.locality;
+
+  let slug = getSlug(locality);
+
+  let existingLocality = await prisma.city.findFirst({
+    where: { slug },
+  });
+
+  if (!!existingLocality) {
+    slug = getSlug([region, locality].join("-"));
+  }
+
+  let existingRegion = await prisma.city.findFirst({
+    where: { slug },
+  });
+
+  if (!!existingRegion) {
+    return {
+      state: "failed",
+      error: "slug already exists",
+      id: profile.id,
+    };
+  }
+
+  const data: any = {
+    id: profile.location.place_id,
+    name: locality,
+    region,
+    slug,
+    countryCode: await getCountryCode(country),
+    description: profile.bio,
+    lat: profile.location.latitude,
+    lng: profile.location.longitude,
+    subscribersCount: profile.watch?.count || 0,
+    viewsCount: profile.viewsCount || 0,
+    createdAt: getDateOrNow(profile.createdAt),
+    updatedAt: getDateOrNow(profile.updatedAt),
+  };
+
+  const result = await prisma.city.create({ data });
+  return {
+    state: "created",
+    id: result.id,
+  };
+}
+
+export async function getUniqueUsername(name: string, count = 1) {
+  let result = getSlug(name, 2, 30);
+
+  if (count > 1) {
+    result = result.substring(0, 30 - count.toString().length);
+    result = result + count;
+    const logger = getLogger("username");
+    logger.debug({ name, result });
+  }
+
+  // Username must be unique
+  const existing = await prisma.profile.findFirst({
+    where: { username: result },
+  });
+
+  if (existing) {
+    return getUniqueUsername(name, count + 1);
+  }
+
+  return result;
+}
+
+export async function addProfile(profile: any) {
+  const existingProfile = await prisma.profile.findFirst({
+    where: { id: profile.id },
+  });
+  const exists = !!existingProfile;
+
+  const username = await getUniqueUsername(profile.username || profile.id);
+
+  const genderMap: any = {
+    Male: "he",
+    Female: "she",
+    Other: "they",
+  };
+
+  const pronounce = genderMap[profile.gender || "Other"];
+
+  const data: any = {
+    id: profile.id,
+    username,
+    name: getNormalizedString(profile.name || username),
+    firebaseUsername: profile.username || "",
+    pronounce,
+    bio: profile.bio || "",
+    story: profile.story || "",
+    firebaseId: profile.id,
+    createdAt: getDateOrNow(profile.createdAt),
+    updatedAt: getDateOrNow(profile.updatedAt),
+    lastLoginAt: getDateOrNull(profile.lastLoginAt),
+    type: profile.type || "FanPage",
+    photo: profile.photo || "",
+    website: profile.website || "",
+    instagram: profile.instagram || "",
+    facebook: profile.facebook || "",
+    youtube: profile.youtube || "",
+    spotify: profile.spotify || "",
+    tiktok: profile.tiktok || "",
+    linkedin: profile.linkedin || "",
+    whatsapp: profile.whatsapp || "",
+    threads: profile.threads || "",
+    twitter: profile.twitter || "",
+    vk: profile.vk || "",
+    telegram: profile.telegram || "",
+    couchsurfing: profile.couchsurfing || "",
+    airbnb: profile.airbnb || "",
+    blablacar: profile.blablacar || "",
+    viewsCount: profile.viewsCount || 0,
+    followersCount: profile.watch?.count || 0,
+  };
+
+  if (profile.location) {
+    data.lat = profile.location.latitude;
+    data.lng = profile.location.longitude;
+    data.placeId = profile.location.place_id;
+  }
+
+  if (profile.place) {
+    const city = await prisma.city.findFirst({
+      where: { id: profile.place },
+    });
+    if (city) {
+      data.cityId = city.id;
+    }
+  }
+
+  const user = await prisma.user.findFirst({ where: { id: profile.id } });
+  if (user) {
+    data.userId = profile.id;
+    data.claimed = true;
+
+    if (user.isDeleted) {
+      data.isDeleted = true;
+      data.deletedAt = user.deletedAt;
+      data.deletedReason = user.deletedReason;
+    }
+  }
+
+  if (data.lastLoginAt) {
+    data.claimed = true;
+  }
+
+  if (profile.createdBy) {
+    const createdBy = await prisma.user.findFirst({
+      where: { id: profile.createdBy },
+    });
+    if (createdBy) {
+      data.createdById = createdBy.id;
+    }
+  }
+
+  const result = await prisma.profile.upsert({
+    where: { id: profile.id },
+    create: data,
+    update: data,
+  });
+
+  if (!exists && profile.styles) {
+    const hashtags = Object.keys(profile.styles);
+    for (const hashtag of hashtags) {
+      const style = await prisma.danceStyle.findUnique({
+        where: { hashtag },
+      });
+
+      if (!style) {
+        console.warn(
+          `Dance style with hashtag '${hashtag}' not found. Skipping.`
+        );
+        continue;
+      }
+
+      await prisma.experience.create({
+        data: {
+          profileId: result.id,
+          styleId: style.id,
+          level: profile.styles[hashtag].level,
+          highlighted: profile.styles[hashtag].highlighted ?? true,
+        },
+      });
+    }
+  }
+
+  return {
+    state: exists ? "updated" : "created",
+    id: result.id,
+    username: data.username,
+    oldUsername: profile.username,
+  };
+}
